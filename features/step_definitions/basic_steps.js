4ad1ea8e352622f46e2e87aa8826b17c862b080f
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-06 12:45:44 +0100

    implimented visit site test, got green

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,11 +1,30 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then, When } = require("cucumber");
 
-Given("I visit the site", function() {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
+Given("I visit the site", async function() {
+return await this.openHomePage();
 });
 
 Then("I should see {string}", function(string) {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
+});
+
+When('I click {string}', async function(string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+});
+  
+Then('I fill in {string} with {string}', async function(string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+});
+  
+Then('I should have {int} contact in my address book', async function(int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+});
+  
+Then('I should not see {string}', async function(string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
 });
