d47f43baba102c56d58ff83156250378c42d6085
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-06 14:02:34 +0100

    implimented should see contacts and should see no contacts, got green

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -5,12 +5,11 @@ After(async function() {
 });
 
 Given("I visit the site", async function() {
-return await this.openHomePage();
+return await this.openHomePage()
 });
 
-Then("I should see {string}", function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
+Then("I should see {string}", async function(content) {
+  return await this.pageHasTextContent(content)
 });
 
 When('I click {string}', async function(string) {
